cda39f0dc089b003536e9153fc9b03ee88055fc8
Bhanu <bhnprtp795@gmail.com>
2021-11-13 22:19:54 +0530

    code refactoring

--- src/main/java/io/betterreads/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/io/betterreads/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -30,6 +30,10 @@ import io.betterreads.betterreadsdataloader.book.Book;
 import io.betterreads.betterreadsdataloader.book.repository.BookRepository;
 import io.betterreads.betterreadsdataloader.connection.DataStaxAstraProperties;
 
+/**
+ * @author Bhanu
+ *
+ */
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsDataLoaderApplication {
@@ -56,14 +60,17 @@ public class BetterReadsDataLoaderApplication {
 	
 	@PostConstruct
 	public void start() {
-//		initAuthors();
+		initAuthors();
 		initWorks();
 	}
 
+	
+	/**
+	 * Method is used to initialize author data 
+	 */
 	private void initAuthors() {
 		Path path = Paths.get(authorDumpLocation);
 		try(Stream<String> lines = Files.lines(path)){
-			
 			lines.forEach(line ->
 			{
 				String jsonString = line.substring(line.indexOf("{"));
@@ -85,56 +92,21 @@ public class BetterReadsDataLoaderApplication {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * Method to initialize book data
+	 */
 	private void initWorks() {
 		Path path = Paths.get(worksDumpLocation);
-		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
 		try(Stream<String> lines = Files.lines(path)){
-			
+			//Limit is set to 50 books for testing
 			lines.limit(50).forEach(line ->
 			{
 				String jsonString = line.substring(line.indexOf("{"));
 				try {
 					JSONObject jsonObject = new JSONObject(jsonString);
 					//Constructing book object
-					Book book = new Book();
-					book.setId(jsonObject.getString("key").replace("/works/", ""));
-					book.setName(jsonObject.optString("title"));
-					JSONObject descriptionJsonObject = jsonObject.optJSONObject("description");
-					if(descriptionJsonObject!=null) {
-						book.setDescription(descriptionJsonObject.optString("value"));
-					}
-					JSONObject publishedObject = jsonObject.optJSONObject("created");
-					if(publishedObject!=null) {
-						String dateStr =publishedObject.getString("value");
-						book.setPublishedDate(LocalDate.parse(dateStr,dateFormatter));
-					}
-					JSONArray coversJsonArray = jsonObject.optJSONArray("covers");
-					if(coversJsonArray!=null) {
-						List<String> coverIds = new ArrayList<>();
-						for(int i=0;i<coversJsonArray.length();i++) {
-							coverIds.add(coversJsonArray.getString(i));
-						}
-						book.setCoverIds(coverIds);
-					}
-					JSONArray authorJsonArray = jsonObject.optJSONArray("authors");
-					if(authorJsonArray!=null) {
-						List<String> authorIds = new ArrayList<>();
-						List<String> authorNames = new ArrayList<>();
-						for(int i=0;i<authorJsonArray.length();i++) {
-							String authorId = authorJsonArray.getJSONObject(i).getJSONObject("author").getString("key")
-									.replace("/authors/", "");
-							authorIds.add(authorId);
-						}
-						book.setAuthorIds(authorIds);
-						authorIds.stream().map(id -> authorRepository.findById(id))
-						.map(optionalAuthor-> 
-						{
-							if(!optionalAuthor.isPresent()) return "Unknown Author";
-							return optionalAuthor.get().getName();
-						}
-								).collect(Collectors.toList());
-						book.setAuthorNames(authorNames);
-					}
+					Book book = prepareBookObject(jsonObject);
 					System.out.println("saving book - "+book.getName());
 					//save using repository
 					bookRepository.save(book);
@@ -145,5 +117,84 @@ public class BetterReadsDataLoaderApplication {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/** Method is used to prepare book object
+	 * @param jsonObject
+	 * @return
+	 * @throws JSONException
+	 */
+	private Book prepareBookObject(JSONObject jsonObject) throws JSONException {
+		Book book = new Book();
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+		book.setId(jsonObject.getString("key").replace("/works/", ""));
+		book.setName(jsonObject.optString("title"));
+		JSONObject descriptionJsonObject = jsonObject.optJSONObject("description");
+		if(descriptionJsonObject!=null) {
+			book.setDescription(descriptionJsonObject.optString("value"));
+		}
+		JSONObject publishedObject = jsonObject.optJSONObject("created");
+		if(publishedObject!=null) {
+			String dateStr =publishedObject.getString("value");
+			book.setPublishedDate(LocalDate.parse(dateStr,dateFormatter));
+		}
+		setCoverIds(jsonObject, book);
+		JSONArray authorJsonArray = jsonObject.optJSONArray("authors");
+		if(authorJsonArray!=null) {
+			List<String> authorIds = getAuthorIds(authorJsonArray);
+			book.setAuthorIds(authorIds);
+			List<String> authorNames = getAuthorNames(authorIds);
+			book.setAuthorNames(authorNames);
+		}
+		return book;
+	}
+
+	/** Method is used to get cover id from JsonObject and set it to book object
+	 * @param jsonObject
+	 * @param book
+	 * @throws JSONException
+	 */
+	private void setCoverIds(JSONObject jsonObject, Book book) throws JSONException {
+		JSONArray coversJsonArray = jsonObject.optJSONArray("covers");
+		if(coversJsonArray!=null) {
+			List<String> coverIds = new ArrayList<>();
+			for(int i=0;i<coversJsonArray.length();i++) {
+				coverIds.add(coversJsonArray.getString(i));
+			}
+			book.setCoverIds(coverIds);
+		}
+	}
+
+	/** Method is used to fetch Author Names by author id
+	 * @param authorIds
+	 * @return
+	 */
+	private List<String> getAuthorNames(List<String> authorIds) {
+		List<String> authorNames = new ArrayList<>();
+		authorIds.stream().map(id -> authorRepository.findById(id))
+		.map(optionalAuthor-> 
+		{
+			if(!optionalAuthor.isPresent()) return "Unknown Author";
+			return optionalAuthor.get().getName();
+		}
+				).collect(Collectors.toList());
+		return authorNames;
+	}
+
+	/**
+	 * Method is used to extract author id  from jsonArray
+	 * @param authorJsonArray
+	 * @return
+	 * @throws JSONException
+	 */
+	private List<String> getAuthorIds(JSONArray authorJsonArray) throws JSONException {
+		List<String> authorIds = new ArrayList<>();
+
+		for(int i=0;i<authorJsonArray.length();i++) {
+			String authorId = authorJsonArray.getJSONObject(i).getJSONObject("author").getString("key")
+					.replace("/authors/", "");
+			authorIds.add(authorId);
+		}
+		return authorIds;
 	}
 }
